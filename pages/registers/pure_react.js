85886c9275141a08b4f835e8481bfc048b62bf46
pphetra <pphetra@gmail.com>
2022-09-12 21:02:58 +0700

    fix bug: tag form อยู่ผิดที่

--- pages/registers/pure_react.js
+++ pages/registers/pure_react.js
@@ -44,88 +44,88 @@ export default function PureReact() {
 
   return (
     <div className="bg-gray-50 h-screen flex flex-col items-center gap-4">
-      <h2 className="mx-auto">Register Form</h2>
-
-      <div className="flex flex-col">
-        <label className="text-sm" htmlFor="username">
-          Username
-        </label>
-        <input
-          className="border rounded"
-          type="text"
-          name="username"
-          id="username"
-          value={state.username}
-          onChange={(e) => {
-            setState({ ...state, username: e.target.value });
-            setErrors({ ...errors, username: "" });
-          }}
-        />
-        {errors.username && (
-          <div className="text-red-500">{errors.username}</div>
-        )}
-      </div>
-
-      <div className="flex flex-col">
-        <label className="text-sm" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="border rounded"
-          type="password"
-          name="password"
-          id="password"
-          value={state.password}
-          onChange={(e) => {
-            setState({ ...state, password: e.target.value });
-            setErrors({ ...errors, password: "" });
-          }}
-        />
-        {errors.password && (
-          <div className="text-red-500">{errors.password}</div>
-        )}
-      </div>
-
-      <div className="flex flex-col">
-        <label className="text-sm" htmlFor="confirmPassword">
-          Confirm Password
-        </label>
-        <input
-          className="border rounded"
-          type="password"
-          name="confirmPassword"
-          id="confirmPassword"
-          value={state.confirmPassword}
-          onChange={(e) => {
-            setState({ ...state, confirmPassword: e.target.value });
-            setErrors({ ...errors, confirmPassword: "" });
-          }}
-        />
-        {errors.confirmPassword && (
-          <div className="text-red-500">{errors.confirmPassword}</div>
-        )}
-      </div>
-
-      <div className="flex flex-col">
-        <div
-          className="flex flex-row gap-4"
-          onChange={(e) => {
-            setState({ ...state, gender: e.target.value });
-            setErrors({ ...errors, gender: "" });
-          }}
-        >
-          <label htmlFor="male">
-            <input type="radio" name="gender" id="male" value="male" />
-            <span className="pl-4">male</span>
-          </label>
-          <label htmlFor="female">
-            <input type="radio" name="gender" id="female" value="female" />
-            <span className="pl-4">female</span>
-          </label>
-        </div>
-      </div>
-
       <form onSubmit={submit}>
+        <h2 className="mx-auto">Register Form</h2>
+
+        <div className="flex flex-col">
+          <label className="text-sm" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="border rounded"
+            type="text"
+            name="username"
+            id="username"
+            value={state.username}
+            onChange={(e) => {
+              setState({ ...state, username: e.target.value });
+              setErrors({ ...errors, username: "" });
+            }}
+          />
+          {errors.username && (
+            <div className="text-red-500">{errors.username}</div>
+          )}
+        </div>
+
+        <div className="flex flex-col">
+          <label className="text-sm" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="border rounded"
+            type="password"
+            name="password"
+            id="password"
+            value={state.password}
+            onChange={(e) => {
+              setState({ ...state, password: e.target.value });
+              setErrors({ ...errors, password: "" });
+            }}
+          />
+          {errors.password && (
+            <div className="text-red-500">{errors.password}</div>
+          )}
+        </div>
+
+        <div className="flex flex-col">
+          <label className="text-sm" htmlFor="confirmPassword">
+            Confirm Password
+          </label>
+          <input
+            className="border rounded"
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            value={state.confirmPassword}
+            onChange={(e) => {
+              setState({ ...state, confirmPassword: e.target.value });
+              setErrors({ ...errors, confirmPassword: "" });
+            }}
+          />
+          {errors.confirmPassword && (
+            <div className="text-red-500">{errors.confirmPassword}</div>
+          )}
+        </div>
+
+        <div className="flex flex-col">
+          <div
+            className="flex flex-row gap-4"
+            onChange={(e) => {
+              setState({ ...state, gender: e.target.value });
+              setErrors({ ...errors, gender: "" });
+            }}
+          >
+            <label htmlFor="male">
+              <input type="radio" name="gender" id="male" value="male" />
+              <span className="pl-4">male</span>
+            </label>
+            <label htmlFor="female">
+              <input type="radio" name="gender" id="female" value="female" />
+              <span className="pl-4">female</span>
+            </label>
+          </div>
+        </div>
+
         <button type="submit" className="border bg-blue-400 rounded px-4 py-2">
           Submit
         </button>
